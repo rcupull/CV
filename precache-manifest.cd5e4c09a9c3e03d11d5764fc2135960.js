3f3744f1bc09db3b16ad90b38401226126307076
Rodobaldo Cupull <rcupull@gmail.com>
2020-06-05 07:52:52 -0400

    Updates

---
+++ precache-manifest.cd5e4c09a9c3e03d11d5764fc2135960.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22bbf067a66036bd4e4f844e35ed9737",
+    "url": "/CV/index.html"
+  },
+  {
+    "revision": "ca4cb5399c6c42c95a25",
+    "url": "/CV/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ca4cb5399c6c42c95a25",
+    "url": "/CV/static/js/2.e69230c0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/CV/static/js/2.e69230c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6778bb59f0de15280a5",
+    "url": "/CV/static/js/main.a9284b87.chunk.js"
+  },
+  {
+    "revision": "27a69f17200fc8340ba7",
+    "url": "/CV/static/js/runtime-main.11b9af39.js"
+  },
+  {
+    "revision": "ac7a62b55964c1418c6259c8b40c8ebe",
+    "url": "/CV/static/media/profile.ac7a62b5.jpg"
+  }
+]);
